4359d99d8fde4d82f91947d40194da17a6ae7a6f
PetriJF <jamespetri28@gmail.com>
2024-08-01 16:46:25 +0100

    Added conditional node based on argument

--- launch/multicam.launch.py
+++ launch/multicam.launch.py
@@ -1,21 +1,24 @@
 import launch
 from launch import LaunchDescription, LaunchService
 from launch.actions import DeclareLaunchArgument
+from launch.conditions import LaunchConfigurationEquals
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from launch.actions import TimerAction
 
 def generate_launch_description():
+    mcpc_camera = LaunchConfiguration('mcpc_camera')
+
     return LaunchDescription([
         # Declare the launch arguments
         DeclareLaunchArgument(
             'mcpc_camera',
-            default_value='Intel Realsense D405',
+            default_value='Luxonis OAK-D Lite',
             description='Camera type for data collection'
         ),
         DeclareLaunchArgument(
             'camera_config_file',
-            default_value='rs_405_camera_config.yaml',
+            default_value='luxonis_oak_d_lite_camera_config.yaml',
             description='Configuration file for the camera'
         ),
 
@@ -61,19 +64,30 @@ def generate_launch_description():
             name='map_controller',
             output='screen'
         ),
+
+        # Conditional node launch based on mcpc_camera value
         Node(
+            condition=LaunchConfigurationEquals('mcpc_camera', 'Intel Realsense D405'),
             package='multicam_pointcloud',
             executable='realsense_multicam_node',
             name='realsense_multicam_node',
             output='screen'
         ),
         Node(
+            condition=LaunchConfigurationEquals('mcpc_camera', 'Luxonis OAK-D Lite'),
+            package='multicam_pointcloud',
+            executable='luxonis_multicam_node.py',
+            name='luxonis_multicam',
+            output='screen'
+        ),
+
+        Node(
             package='multicam_pointcloud',
             executable='data_collection_node.py',
             name='data_collector',
             output='screen',
             parameters=[
-                {'mcpc_camera': LaunchConfiguration('mcpc_camera')},
+                {'mcpc_camera': mcpc_camera},
                 {'camera_config_file': LaunchConfiguration('camera_config_file')}
             ]
         ),
