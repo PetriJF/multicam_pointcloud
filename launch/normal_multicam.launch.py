96c5b2dfb5486ba2b5337d54a21931f469c1b5f3
PetriJF <jamespetri28@gmail.com>
2024-08-12 14:19:15 +0100

    Added better launch options

--- launch/normal_multicam.launch.py
+++ launch/normal_multicam.launch.py
@@ -7,19 +7,12 @@ from launch_ros.actions import Node
 from launch.actions import TimerAction
 
 def generate_launch_description():
-    mcpc_camera = LaunchConfiguration('mcpc_camera')
-
     return LaunchDescription([
         # Declare the launch arguments
         DeclareLaunchArgument(
             'mcpc_camera',
             default_value='Luxonis OAK-D Lite',
             description='Camera type for data collection'
-        ),
-        DeclareLaunchArgument(
-            'camera_config_file',
-            default_value='luxonis_oak_d_lite_camera_config.yaml',
-            description='Configuration file for the camera'
         ),
 
         Node(
@@ -79,19 +72,6 @@ def generate_launch_description():
             executable='luxonis_multicam_node.py',
             name='luxonis_multicam',
             output='screen'
-        ),
-
-        Node(
-            package='multicam_pointcloud',
-            executable='data_collection_node.py',
-            name='data_collector',
-            output='screen',
-            parameters=[
-                {'mcpc_camera': mcpc_camera},
-                {'camera_config_file': LaunchConfiguration('camera_config_file')},
-                {'image_save_path': '~/farmbot_images/'},
-                {'daily_measurement_count': 0}
-            ]
         ),
 
         # Delay for 15 seconds
