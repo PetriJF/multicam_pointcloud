58fdbda61dd656f0c27a33489346bc247b8e11e8
PetriJF <jamespetri28@gmail.com>
2024-08-19 11:59:58 +0100

    Formatting and server response message

--- multicam_pointcloud/luxonis_multicam.py
+++ multicam_pointcloud/luxonis_multicam.py
@@ -27,11 +27,11 @@ class LuxonisMulticam(Node):
         '''
         super().__init__('LuxonisMulticam')
 
-        # TESTING
+        ### Extend node to data manager module
         self.img_mngr_ = CamDataManager(self)
         
-
-        self.bridge = CvBridge()  # Bridge to convert between ROS and OpenCV images
+        ### Camera variables and objects
+        self.bridge = CvBridge()
         self.controlQueues = []
         self.stillQueues = []
         self.devices = []
@@ -39,6 +39,7 @@ class LuxonisMulticam(Node):
         self.rightQueues = []
         self.stereoQueues = []
         self.exit_stack = contextlib.ExitStack()  # Create an ExitStack as an attribute
+
 
         self.config_directory = os.path.join(get_package_share_directory('multicam_pointcloud'), 'config')
         camera_config_file = 'luxonis_oak_d_lite_camera_config.yaml'
@@ -65,39 +66,6 @@ class LuxonisMulticam(Node):
         # self.capture_depth_stacks()
 
         self.get_logger().info('Luxonis Multicam Node initialized...')
-        
-    # def test_publish(self):
-    #     for camera_id, publisher in self.rgb_publishers.items():
-    #         dummy_image = np.zeros((480, 640, 3), dtype=np.uint8)  # Create a dummy image
-    #         image_message = ImageMessage()
-    #         image_message.image = self.bridge.cv2_to_imgmsg(dummy_image, encoding='bgr8')
-    #         image_message.id = 1
-    #         image_message.focus = 150
-    #         publisher.publish(image_message)
-    #         self.get_logger().info(f"Published test image to cam_id: {camera_id}")
-
-    def load_from_yaml(self, path, file_name):
-        full_path = os.path.join(path, file_name)
-        if not os.path.exists(full_path):
-            self.get_logger().warn(f"File path is invalid: {full_path}")
-            return None
-        
-        with open(full_path, 'r') as yaml_file:
-            try:
-                return yaml.safe_load(yaml_file)
-            except yaml.YAMLError as e:
-                self.get_logger().warn(f"Error reading YAML file: {e}")
-                return None
-
-    # def create_publishers(self):
-    #     '''
-    #     Creates individual publishers for each camera's RGB, mono, and depth images
-    #     '''
-    #     for device in self.devices:
-    #         camera_id = device.getMxId()
-    #         self.rgb_publishers[camera_id] = self.create_publisher(ImageMessage, f'rgb_{camera_id}', 10)
-    #         self.mono_publishers[camera_id] = self.create_publisher(ImageMessage, f'mono_{camera_id}', 10)
-    #         self.depth_publishers[camera_id] = self.create_publisher(ImageMessage, f'depth_{camera_id}', 10)
 
     def configure_stereo_depth(self, stereo):
         '''
@@ -116,7 +84,7 @@ class LuxonisMulticam(Node):
         config.postProcessing.spatialFilter.enable = True
         stereo.initialConfig.set(config)
         
-    def createPipeline(self, idx):
+    def create_pipeline(self, idx):
         pipeline = dai.Pipeline()
         camRgb = pipeline.create(dai.node.ColorCamera)
         camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_4_K)
@@ -124,8 +92,8 @@ class LuxonisMulticam(Node):
 
         monoRight = pipeline.create(dai.node.MonoCamera)
         monoLeft = pipeline.create(dai.node.MonoCamera)
-        monoRight.setCamera("right")
-        monoLeft.setCamera("left")
+        monoRight.setCamera('right')
+        monoLeft.setCamera('left')
         monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_480_P)
         monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_480_P)
 
@@ -163,7 +131,7 @@ class LuxonisMulticam(Node):
         '''
         deviceInfos = dai.Device.getAllAvailableDevices()
         if len(deviceInfos) < 1:
-            self.get_logger().info("Please connect at least 1 device.")
+            self.get_logger().info('Please connect at least 1 device.')
             exit(1)
 
         for idx, deviceInfo in enumerate(deviceInfos):
@@ -171,85 +139,45 @@ class LuxonisMulticam(Node):
                 usb_speed = dai.UsbSpeed.HIGH
                 device = self.exit_stack.enter_context(dai.Device(deviceInfo, usb_speed))
                 self.devices.append(device)
-                pipeline = self.createPipeline(idx)
+                pipeline = self.create_pipeline(idx)
                 device.startPipeline(pipeline)
                 controlQueue = device.getInputQueue(f'control-{idx}')
                 stillQueue = device.getOutputQueue(f'still-{idx}', maxSize=1, blocking=False)
-                leftQueue = device.getOutputQueue(f"left-{idx}", maxSize=1, blocking=False)
-                rightQueue = device.getOutputQueue(f"right-{idx}", maxSize=1, blocking=False)
-                stereoQueue = device.getOutputQueue(f"stereo-{idx}", maxSize=1, blocking=False)
+                leftQueue = device.getOutputQueue(f'left-{idx}', maxSize=1, blocking=False)
+                rightQueue = device.getOutputQueue(f'right-{idx}', maxSize=1, blocking=False)
+                stereoQueue = device.getOutputQueue(f'stereo-{idx}', maxSize=1, blocking=False)
                 self.controlQueues.append(controlQueue)
                 self.stillQueues.append(stillQueue)
                 self.leftQueues.append(leftQueue)
                 self.rightQueues.append(rightQueue)
                 self.stereoQueues.append(stereoQueue)
-                self.get_logger().info(f"Connected to device {idx+1} with MXID: {device.getMxId()}")
+                self.get_logger().info(f'Connected to device {idx+1} with MXID: {device.getMxId()}')
             except Exception as e:
-                self.get_logger().info(f"Failed to connect to device {idx+1}, error: {e}")
+                self.get_logger().info(f'Failed to connect to device {idx+1}, error: {e}')
                 exit(1)
-        
-            
-    def publish_images(self, rgb_frame=None, focus=None, id=None, mono_frame=None, depth_frame=None):
-        '''
-        Publish RGB, mono, and depth frames if available
-        '''
-        try:
-            camera_id = self.devices[id-1].getMxId()
-            if rgb_frame is not None:
-                if rgb_frame.shape[2] == 3:  # Ensure the frame has 3 channels
-                    self.image_message_.image = self.bridge.cv2_to_imgmsg(rgb_frame, encoding='bgr8')
-                    self.image_message_.id = id
-                    self.image_message_.focus = focus
-                    if camera_id in self.rgb_publishers:
-                        self.rgb_publishers[camera_id].publish(self.image_message_)
-                    else:
-                        self.get_logger().error(f"Publisher for camera ID {camera_id} not found.")
-
-                    #self.rgb_publishers[camera_id].publish(self.image_message_)
-                    self.get_logger().info(f"Published rgb image to cam_id: {camera_id}")
-
-                else:
-                    self.get_logger().error(f"RGB frame for Camera {id} does not have 3 channels")
-            if mono_frame is not None:
-                self.image_message_.image = self.bridge.cv2_to_imgmsg(mono_frame, encoding='mono8')
-                self.image_message_.id = id
-                self.image_message_.focus = focus
-                self.mono_publishers[camera_id].publish(self.image_message_)
-                self.get_logger().info(f"Published mono image to cam_id: {camera_id}")
-            if depth_frame is not None:
-                self.image_message_.image = self.bridge.cv2_to_imgmsg(depth_frame, encoding='16UC1')
-                self.image_message_.id = id
-                self.image_message_.focus = focus
-                self.depth_publishers[camera_id].publish(self.image_message_)
-                self.get_logger().info(f"Published depth image to cam_id: {camera_id}")
-        except Exception as e:
-            self.get_logger().error(f"Failed to publish images: {e}")
         
     def clamp(num, v0, v1):
         return max(v0, min(num, v1))
-
-    def cleanup(self, devices):
-        for device in devices:
-            device.close()
 
     def capture_depth_stacks_server(self, request, response):
         if request.data == 'TAKE':
             if 1 > 2:   # TODO add checks here
                 response.data = 'FAILED'
                 return response
-            self.capture_depth_stacks()
-            response.data == 'SUCCESS'
+            
+            result = self.capture_depth_stacks()
+            response.data == 'FINISHED' if result else 'FAILED'
             return response
 
     def capture_depth_stacks(self):
         for controlQueue in self.controlQueues:
             try:
-                self.get_logger().info("Sending initial manual focus to control queue")
+                self.get_logger().info('Sending initial manual focus to control queue')
                 ctrl = dai.CameraControl()
                 ctrl.setManualFocus(150)
                 controlQueue.send(ctrl)
             except Exception as e:
-                self.get_logger().info(f"Error setting initial manual focus: {type(e).__name__}: {e}")
+                self.get_logger().info(f'Error setting initial manual focus: {type(e).__name__}: {e}')
 
         cv2.waitKey(500)
         lens_steps_a = self.cam_config_data[f'camera_{self.devices[0].getMxId()}']['lens_steps']
@@ -258,7 +186,6 @@ class LuxonisMulticam(Node):
                 cam_id = self.devices[i].getMxId()
                 lens_position = self.cam_config_data[f'camera_{cam_id}']['lens_steps'][j]
                 try:
-                    #self.get_logger().info(f"Sending capture still to control queue for Camera {i+1}")
                     ctrl = dai.CameraControl()
                     ctrl.setCaptureStill(True)
                     self.controlQueues[i].send(ctrl)
@@ -275,30 +202,25 @@ class LuxonisMulticam(Node):
                                 #new_dimensions = (width // 2, height // 2)
                                 #rgb_image = cv2.resize(rgb_image, new_dimensions, interpolation=cv2.INTER_AREA)
                                 self.img_mngr_.save_img('rgb', cam_id, rgb_image, lens_position)
-                                #self.publish_images(rgb_frame=rgb_image, id=i+1, focus=lens_position)
-                                self.get_logger().info(f"Camera {i+1} - Setting manual focus, lens position: {lens_position}")
+                                self.get_logger().info(f'Camera {i+1} - Setting manual focus, lens position: {lens_position}')
                                 ctrl = dai.CameraControl()
                                 ctrl.setManualFocus(lens_position)
                                 self.controlQueues[i].send(ctrl)
                             else:
-                                self.get_logger().info(f"Camera {i+1} - Failed to decode frame data")
+                                self.get_logger().info(f'Camera {i+1} - Failed to decode frame data')
                         else:
-                            self.get_logger().info(f"Camera {i+1} - Received empty frame data")
+                            self.get_logger().info(f'Camera {i+1} - Received empty frame data')
 
-                        # Clear the stillQueue to avoid filliFng up the buffer
+                        # Clear the stillQueue to avoid filling up the buffer
                         stillQueue.tryGetAll()
 
                 except RuntimeError as e:
-                    self.get_logger().info(f"Camera {i+1} encountered an error while sending control command: {e}")
+                    self.get_logger().info(f'Camera {i+1} encountered an error while sending control command: {e}')
                     self.cleanup(self.devices)
-                    exit(1)
-                key = cv2.waitKey(1)
-                if key == ord('q'):
-                    self.get_logger().info(f"Pressed q, exiting")
-                    self.cleanup(self.devices)
-                    exit(0)
+                    self.destroy_node()
+                    return False 
 
-        #do stereo depth capture here
+        # do stereo depth capture here
         for i, leftQueue in enumerate(self.leftQueues):
             cam_id = self.devices[i].getMxId()
             rightQueue = self.rightQueues[i]
@@ -306,14 +228,33 @@ class LuxonisMulticam(Node):
             rightFrame = rightQueue.get()
             self.img_mngr_.save_img('mono', cam_id, leftFrame.getCvFrame(), focus=-10)
             self.img_mngr_.save_img('mono', cam_id, rightFrame.getCvFrame(), focus=-11)
-            #self.publish_images(mono_frame=leftFrame.getCvFrame(), id=i+1, focus=-10)
-            #self.publish_images(mono_frame=rightFrame.getCvFrame(), id=i+1, focus=-11)
         for i, stereoQueue in enumerate(self.stereoQueues):
             cam_id = self.devices[i].getMxId()
             stereoFrame = stereoQueue.get()
             self.img_mngr_.save_img('depth', cam_id, stereoFrame.getCvFrame(), focus=-9)
-            #self.publish_images(depth_frame=stereoFrame.getCvFrame(), id=i+1, focus=-9)
-        self.get_logger().info("Depth stack capture complete.")
+        
+        self.get_logger().info('Depth stack capture complete.')
+        return True  
+
+    def load_from_yaml(self, path, file_name):
+        '''
+        Returns a yaml file from the selected path and file name
+        '''
+        full_path = os.path.join(path, file_name)
+        if not os.path.exists(full_path):
+            self.get_logger().warn(f'File path is invalid: {full_path}')
+            return None
+        
+        with open(full_path, 'r') as yaml_file:
+            try:
+                return yaml.safe_load(yaml_file)
+            except yaml.YAMLError as e:
+                self.get_logger().warn(f'Error reading YAML file: {e}')
+                return None
+
+    def cleanup(self, devices):
+        for device in devices:
+            device.close()
 
     def destroy_node(self):
         super().destroy_node()
