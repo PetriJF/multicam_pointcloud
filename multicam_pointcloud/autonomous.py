ba534a92c5b424133e43ff5ec3904ef37300b06a
AURAProj <aura.team.gh@gmail.com>
2024-09-05 08:58:02 +0000

    bugfix for times

--- multicam_pointcloud/autonomous.py
+++ multicam_pointcloud/autonomous.py
@@ -43,15 +43,15 @@ class PointCloudController(Node):
         if self.daily_iter_ == -1:
             pass
         elif self.daily_iter_ == 2:
-            if current_time == '7:50' or current_time == '14:00':
+            if current_time == '07:50' or current_time == '14:00':
                 self.msg_.data = 'H_0'
                 self.input_pub_.publish(self.msg_)
                 self.get_logger().info(f'{current_time} -- Homing the robot!')
-            elif current_time == '7:40' or current_time == '13:50':
+            elif current_time == '07:40' or current_time == '13:50':
                 self.msg_.data = 'C_0 Y'
                 self.input_pub_.publish(self.msg_)
                 self.get_logger().info(f'{current_time} -- Calibrating the Y-Axis!')
-            elif current_time == '8:00' or current_time == '14:10':
+            elif current_time == '08:00' or current_time == '14:10':
                 self.msg_.data = self.final_sequence
                 self.sequencer_pub_.publish(self.msg_)
                 self.get_logger().info(f'{current_time} -- Running the 3-Camera Imager sequence!')
